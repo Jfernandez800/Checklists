34b8d658afa045c36f2c7609300844de3079bb43
James Fernandez <jfernandez800@gmail.com>
2023-08-25 03:05:22 -0400

    -Modified list to have more rows. -Set tag to 1000 -Added method to check and uncheck from the list. -Gave lists a method to add grey background when selecting item in list.

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -21,17 +21,48 @@ class ChecklistViewController: UITableViewController {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        return 1 }// tells table that we have one row.
+        return 100 }// tells table that we have 100 rows.
     
     //this method grabs a copy of the prototype cell and returns it.
     override func tableView(
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell {
+        ) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "ChecklistItem",
             for: indexPath)
-        return cell
+      
+        let label = cell.viewWithTag(1000) as! UILabel
+            
+            //Points to the row that the cell is intended for.
+            if indexPath.row % 5 == 0 {
+              label.text = "Walk the dog"
+            } else if indexPath.row % 5 == 1 {
+              label.text = "Brush my teeth"
+            } else if indexPath.row % 5 == 2 {
+              label.text = "Learn iOS development"
+            } else if indexPath.row % 5 == 3 {
+              label.text = "Soccer practice"
+            } else if indexPath.row % 5 == 4 {
+              label.text = "Eat ice cream"
+            }
+        // End of new code block
+        return cell }
+    
+    // MARK: - Table View Delegate
+    //if user taps row.
+    override func tableView(
+      _ tableView: UITableView,
+      didSelectRowAt indexPath: IndexPath
+    ){
+    if let cell = tableView.cellForRow(at: indexPath) {
+        if cell.accessoryType == .none {
+          cell.accessoryType = .checkmark
+    } else {
+          cell.accessoryType = .none
+        }
+    }
+      tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
